4d235e5c3e3c45ffa7c28e4a5bd4af86b36bb8ad
M. ANDREI <123612995+MYSTICCC09@users.noreply.github.com>
2023-07-21 22:59:21 +0800

    Update class-vending-inventory.cs
    Signed-off-by: M. ANDREI <123612995+MYSTICCC09@users.noreply.github.com>

--- class-vending-inventory.cs
+++ class-vending-inventory.cs
@@ -6,19 +6,21 @@ using System.Collections.Generic;
 
 namespace RefreshmentDrinkSystem
 {
-    class VendingMachine //vending machine inventory, available drinks & balance
-                         //allows to add more drinks po from the drinks available at main prog
+    class VendingMachine //vending machine inventory, available drinks & balance.
+                         //allows to add more drinks po from the drinks available at main prog.
     {
-        public List<Drink> drinks;
-        public decimal balance;
+        public List<Drink> drinks; // A list that holds the available drinks in the vending machine.
+        public decimal balance; // The current balance in the vending machine (total funds available for purchase.
 
-        public VendingMachine()
+        public VendingMachine() // Constructor para po  mag initialize yung system ko ng with an empty list of drinks and a zero balance.
+                                // ang catch po dun sa main code kaya meron na siyang available list of drinks ay gawa po nung create dummmy data.
         {
-            drinks = new List<Drink>();
+            drinks = new List<Drink>();  
             balance = 0;
         }
 
-        public void AddDrink(Drink drink)
+                                // methods po para ma define yung drink 
+        public void AddDrink(Drink drink) 
         {
             drinks.Add(drink);
         }
@@ -28,9 +30,9 @@ namespace RefreshmentDrinkSystem
             return drinks;
         }
 
-        public decimal PurchaseDrink(string drinkName, int quantity)
+        public decimal PurchaseDrink(string drinkName, int quantity) //  para po pag may bumili ma set ang name and quantity.
         {
-            Drink drink = drinks.Find(d => d.Name == drinkName);
+            Drink drink = drinks.Find(d => d.Name == drinkName); // lambda expression - checks property of drinks in the list if equal to the parameter po.
 
             if (drink != null && drink.Quantity >= quantity)
             {
